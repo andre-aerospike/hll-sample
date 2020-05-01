ceb153292e1c5cdf0bb135e42e5c659a581a2e3f
Andre Chang <achang@aerospike.com>
2020-05-01 12:24:55 -0700

    Some cleanup

--- src/ExampleBase.java
+++ src/ExampleBase.java
@@ -1,8 +1,14 @@
 //import java.util.List;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+import java.util.Map.Entry;
 import java.util.Random;
 
 import com.aerospike.client.AerospikeClient;
+import com.aerospike.client.Value;
+import com.aerospike.client.Value.IntegerValue;
 import com.aerospike.client.policy.WritePolicy;
 
 import common.Console;
@@ -12,7 +18,7 @@ public abstract class ExampleBase
 	String m_host = "192.168.56.10";
 	int m_port = 3000;
 	String m_namespace = "test";
-	String m_set;
+	String m_set = "set";
 
 	Console m_console;
 	WritePolicy m_writePolicy;
@@ -20,10 +26,8 @@ public abstract class ExampleBase
 
 	Random m_random;
 
-	public ExampleBase(String set)
+	public ExampleBase()
 	{
-        m_set = set;
-
         m_client = new AerospikeClient(m_host, m_port);
 		m_console = new Console();
 		m_writePolicy = new WritePolicy();
@@ -32,6 +36,89 @@ public abstract class ExampleBase
 		m_random.setSeed(12345);	// So we can get consistent runs
 
 	}
+
+
+
+	// Data generation modelling
+	enum GroupType
+	{
+		Even,
+		Odd,
+		All,
+	}
+
+	int GetValue(GroupType type, int min, int max)
+	{
+		int x = m_random.nextInt(max - min) + min;
+		switch (type)
+		{
+		case Odd:
+			x = x | 1;
+			break;
+		case Even:
+			x = x & ~1;
+			break;
+		case All:
+		default:
+			break;
+		}
+		return x;
+	}
+
+
+	// Dataset definitions
+	final class GroupParams
+	{
+		int m_count;
+		GroupType m_type;
+		int	m_min;
+		int m_max;
+		boolean m_hide_range;
+
+		public GroupParams(int count, GroupType type, int min, int max, boolean hide_range)
+		{
+			m_count = count;
+			m_type = type;
+			m_min = min;
+			m_max = max;
+			m_hide_range = hide_range;
+		}
+	}
+
+	int m_MAX    =  100000;
+	int m_SIZE   = 1000000; // Number of elements in each group
+
+	Entry<String, GroupParams> e;
+	final Map<String, GroupParams> m_groupdef = Map.ofEntries(
+			//		  name				   count		values			min		max			hide range
+			Map.entry("A", new GroupParams(m_SIZE,		GroupType.All,		0,	m_MAX,		false)),
+			Map.entry("B", new GroupParams(m_SIZE,		GroupType.Even,		0,	m_MAX,		false)),
+			Map.entry("C", new GroupParams(m_SIZE,		GroupType.Odd,		0,	m_MAX,		false)),
+			Map.entry("D", new GroupParams(m_SIZE,		GroupType.All,		0,	m_MAX/2,	true)),
+			Map.entry("E", new GroupParams(m_SIZE/20,	GroupType.All,	 4000,	8000,		true))
+			);
+
+	// Get (generate) the data for a named group dataset
+	List<Value> GetGroup(String name)
+	{
+		GroupParams params = m_groupdef.get(name);
+
+		List<Value> list = new ArrayList<>();
+		int i=0;
+		if (params.m_hide_range)
+		{
+			list.add(new IntegerValue(0));
+			list.add(new IntegerValue(m_MAX-1));
+			i += 2;
+		}
+		for (; i<params.m_count; i++)
+		{
+			list.add(new IntegerValue(GetValue(params.m_type, params.m_min, params.m_max)));
+		}
+		return list;
+	}
+
+
 
 	abstract boolean Run();
 
